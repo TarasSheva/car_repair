d201675b71c9d8d6b35a0a736d8f8e352ee9f477
TarasSheva <tarasgsheva@gmail.com>
2020-09-25 13:29:16 +0300

    release v1

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// паралакс эффект
+$('.parallax-home').parallax({
+    imageSrc: 'img/fon_11.png',
+    speed: 0
+});
+
+$('.parallax-services').parallax({
+    imageSrc: 'img/fon_2.png'
+});
+
+$('.parallax_about').parallax({
+    imageSrc: 'img/fon_3.png'
+});
+
+$('.parallax-fon_about').parallax({
+    imageSrc: 'img/fon_4.png'
+});
+
+$('.parallax_contacts').parallax({
+    imageSrc: 'img/fon_3.png'
+});
